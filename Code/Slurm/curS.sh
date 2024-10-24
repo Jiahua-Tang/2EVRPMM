9c8bf9532760d36aeedf413ec03d2a6e2c46a7a2
jiahua.tang@etu-upsaclay.fr <tangj@ruche02.cluster>
2024-10-24 20:51:39 +0200

    Slurm output naming

--- Code/Slurm/curS.sh
+++ Code/Slurm/curS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=C101C70Q80
-#SBATCH --output=./Result/outputSlurm/%x.o%j 
+#SBATCH --output=./Result/outputSlurm/%j.%x 
 #SBATCH --time=00:50:00 
 #SBATCH --ntasks=10
 #SBATCH --partition=cpu_med
@@ -13,6 +13,8 @@
 julia configure_env.jl
 
 # Run code
-julia  2EVRPMM-V7-z-2index.jl C101-70.txt 600 80 45  
-echo "Your job is finished" | mail -s "Job Complete" tangjiahua98@gmail.com
+julia 2EVRPMM-V7-2index.jl
+#julia  2EVRPMM-V7-z-2index.jl C101-70.txt 600 80 45  
+#exit()
+#echo "Your job is finished" | mail -s "Job Complete" tangjiahua98@gmail.com
 
