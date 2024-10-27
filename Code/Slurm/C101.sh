4a018b69a9dc3bd210742a1721b45a02a395fbaf
jiahua.tang@etu-upsaclay.fr <tangj@ruche01.cluster>
2024-10-27 22:18:30 +0100

    Test result

--- Code/Slurm/C101.sh
+++ Code/Slurm/C101.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=C101C50Q50R
+#SBATCH --job-name=C101MM10P16
 #SBATCH --output=/gpfs/users/tangj/2EVRPMM/Code/Result/outputSlurm/%j.%x.out
 #SBATCH --error=/gpfs/users/tangj/2EVRPMM/Code/Result/outputSlurm/%j.%x.err  # Capture standard error in a separate file
-#SBATCH --time=01:15:00 
-#SBATCH --ntasks=1                              # Since Julia typically uses multithreading, setting ntasks to 1
+#SBATCH --time=04:00:00 
+#SBATCH --ntasks=1   # Since Julia typically uses multithreading, setting ntasks to 1
 #SBATCH --cpus-per-task=10                      # Ensure you're using all 10 CPUs
-#SBATCH --partition=cpu_med
+#SBATCH --partition=cpu_prod
 
 # Load Julia module (uncomment this if the environment requires it)
 #module load julia/1.4.0/gcc-9.2.0
@@ -19,4 +19,5 @@ julia /gpfs/users/tangj/2EVRPMM/Code/configure_env.jl
 
 # Run the main Julia code
 # julia /gpfs/users/tangj/2EVRPMM/Code/2EVRPMM-V7-z-2index.jl 100/C101.txt , 50 180
-julia /gpfs/users/tangj/2EVRPMM/Code/2EVRPMM-V7-z-2index.jl 50/C101_50.txt 600 50 60 r
+julia /gpfs/users/tangj/2EVRPMM/Code/2EVRPMM-compare-result-parking.jl 100/C101.txt 600 50 220
+
