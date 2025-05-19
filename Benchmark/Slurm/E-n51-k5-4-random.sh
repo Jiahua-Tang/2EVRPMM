9d41166d5361331c52631c602049232aa3c4ee98
Jiahua-Tang <tangjiahua98@gmail.com>
2025-05-19 23:25:27 +0200

    update slurm script

--- Benchmark/Slurm/E-n51-k5-4-random.sh
+++ Benchmark/Slurm/E-n51-k5-4-random.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=E-n33-k4
+#SBATCH --job-name=E-n51-k5
 #SBATCH --output=/gpfs/workdir/tangj/2EVRPMM/Benchmark/Result/OutputSlurm/output/E-n51-k5/%j.random.%x.out
 #SBATCH --error=/gpfs/workdir/tangj/2EVRPMM/Benchmark/Result/OutputSlurm/err/E-n51-k5/%j.random.%x.err  # Capture standard error in a separate file
 #SBATCH --time=06:00:00 
 #SBATCH --ntasks=1   # Since Julia typically uses multithreading, setting ntasks to 1
 #SBATCH --cpus-per-task=10                      # Ensure you're using all 10 CPUs
 #SBATCH --partition=cpu_prod
+#SBATCH --mail-type=ALL
 
 # Load Julia module (uncomment this if the environment requires it)
 #module load julia/1.4.0/gcc-9.2.0
